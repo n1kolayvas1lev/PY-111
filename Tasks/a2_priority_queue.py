48435654a3d91496dfc4ae734dc6d81ccd6b979b
n1kolayvas1lev <ckb01@ya.ru>
2022-02-17 11:50:40 +0300

    a2_priority_queue 17.02.2022 Tests OK

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -8,15 +8,17 @@ from typing import Any
 
 class PriorityQueue:
     def __init__(self):
-        ...  # todo для очереди можно использовать python dict
+        self.queue = {i: [] for i in range(11)}  # Для очереди можно использовать python dict.
 
     def enqueue(self, elem: Any, priority: int = 0) -> None:
         """
         Operation that add element to the end of the queue
 
+        :param priority: priority of element in queue
         :param elem: element to be added
         :return: Nothing
         """
+        self.queue[priority].append(elem)
         return None
 
     def dequeue(self) -> Any:
@@ -25,15 +27,21 @@ class PriorityQueue:
 
         :return: dequeued element
         """
+        for i in range(11):
+            if len(self.queue[i]) > 0:
+                return self.queue[i].pop(0)
         return None
 
     def peek(self, ind: int = 0, priority: int = 0) -> Any:
         """
         Allow you to see at the element in the queue without dequeuing it
 
+        :param priority: priority of element in queue
         :param ind: index of element (count from the beginning)
         :return: peeked element
         """
+        if len(self.queue[priority]) > 0:
+            return self.queue[priority][ind]
         return None
 
     def clear(self) -> None:
@@ -42,4 +50,5 @@ class PriorityQueue:
 
         :return: None
         """
+        self.queue = {i: [] for i in range(11)}
         return None
