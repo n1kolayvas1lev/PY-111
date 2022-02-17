1c3a702d1ced091cadff992b749dcab17b3c7419
n1kolayvas1lev <ckb01@ya.ru>
2022-02-17 13:56:57 +0300

    a3_check_brackets 17.02.2022 Tests OK Позволяет вычленять последовательности типа: ()))(((). Добавлено соответствующее тестирование.

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -8,10 +8,13 @@ def check_brackets(brackets_row: str):
 
     if brackets_row != '' and (brackets_row[0] == ')' or brackets_row[-1] == '('
                                or brackets_row.count('(') != brackets_row.count(')')):
-        return False
+        return False  # Уже проходит все тесты.
+    open_closed_brackets_counter = 0
+    for _ in brackets_row:
+        if _ == '(':
+            open_closed_brackets_counter += 1
+        if _ == ')':
+            open_closed_brackets_counter -= 1
+        if open_closed_brackets_counter < 0:
+            return False  # Позволяет вычленять последовательности типа: ()))((().
     return True
-
-
-if __name__ == "__main__":
-    check_brackets("(()(")
-    check_brackets("")
