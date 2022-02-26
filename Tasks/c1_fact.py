79da56c25448228a8a2ba828e689d88c8f1eb8de
n1kolayvas1lev <ckb01@ya.ru>
2022-02-26 12:31:35 +0300

    c1_fact

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -4,8 +4,11 @@ def factorial_recursive(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return factorial_recursive(n - 1) * n
 
 
 def factorial_iterative(n: int) -> int:
@@ -15,5 +18,32 @@ def factorial_iterative(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    f = 1
+    for _ in range(1, n + 1):
+        f *= _
+    return f
+
+
+def factorial_generator(n: int):
+    if n < 0:
+        raise ValueError
+    f = 1
+    for _ in range(0, n + 1):
+        if _ == 0:
+            yield f
+        else:
+            f *= _
+            yield f
+
+
+if __name__ == '__main__':
+    f = factorial_generator(5)
+    print(next(f))
+    print(next(f))
+    print(next(f))
+    print(next(f))
+    print(next(f))
+    print(next(f))
+
