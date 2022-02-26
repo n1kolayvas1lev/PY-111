cc31f863a9ee33d90ae292f08ee614b2e8559f40
n1kolayvas1lev <ckb01@ya.ru>
2022-02-26 19:59:48 +0300

    c2_recursive_binary_search

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -1,13 +1,31 @@
 from typing import Sequence, Optional
 
 
-def binary_search(elem: int, arr: Sequence) -> Optional[int]:
+def binary_search(elem: int, arr: Sequence,
+                  left_border=None, right_border=None) -> Optional[int]:
     """
     Performs binary search of given element inside of array (using recursive way)
 
+    :param left_border: Left search border index.
+    :param right_border: Right search border index.
     :param elem: element to be found
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    if left_border is None:
+        left_border = 0
+    if right_border is None:
+        right_border = len(arr) - 1
+    if left_border > right_border:
+        return None  # No element found.
+    middle_index = (left_border + right_border) // 2
+    middle_value = arr[middle_index]
+    if middle_value == elem:
+        return middle_index
+    elif middle_value < elem:
+        new_left_border = middle_index + 1
+        return binary_search(elem, arr, new_left_border, right_border)
+    elif middle_value > elem:
+        new_right_border = middle_index - 1
+        return binary_search(elem, arr, left_border, new_right_border)
+
