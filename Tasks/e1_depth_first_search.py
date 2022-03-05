a71e4804bd5abf2646acc34d6b6f8d9c6bab6b01
n1kolayvas1lev <ckb01@ya.ru>
2022-03-05 20:00:26 +0300

    e1_depth_first_search

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,14 +1,35 @@
 from typing import Hashable, List
 import networkx as nx
+from collections import deque
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     """
-    Do an depth-first search and returns list of nodes in the visited order
+    Do an depth-first search and returns list
+    of nodes in the visited order
 
     :param g: input graph
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
+    # g.nodes  # Все узлы графа.
+    neighbours = g[start_node]
+
+    path_nodes = []  # Полностью сгоревшие
+    visited_nodes = {node: False for node in g.nodes}  # Посещённые узлы.
+
+    wait_nodes = deque()  # Очередь из горящих узлов, ожидающих поджечь своих соседей.
+    wait_nodes.append(start_node)  # Добавляем стартовый эл-т в начало очереди.
+    visited_nodes[start_node] = True
+
+    while wait_nodes:
+        current_node = wait_nodes.pop()  # Забираем узел из конца очереди.
+        neighbours = g[current_node]
+        for neighbour in neighbours:
+            if not visited_nodes[neighbour]:
+                wait_nodes.append(neighbour)
+                visited_nodes[neighbour] = True
+
+        path_nodes.append(current_node)  # Полностью сожгли.
     print(g, start_node)
     return list(g.nodes)
